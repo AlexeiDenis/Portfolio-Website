f58e69cc24a4ba744905ada741ea55667e022d89
AlexeiDenis <83495616+AlexeiDenis@users.noreply.github.com>
2022-05-08 20:14:12 +0300

    99% finished

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 let btn = document.getElementById('toggler')
 let col = document.getElementById('decoration')
+let image = document.getElementById('imgdecor')
 let c = true;
-console.log("working...??")
+let y = true;
+
+
+image.addEventListener('click', () => {
+    if (y) { y = false; image.src = "./imagini/sticker.webp" } else { y = true; image.src = "./imagini/avatar.webp" }
+
+})
+
 btn.addEventListener('click', () => {
-    if (c) { col.src = "./imagini/moon.svg"; c = false; } else { col.src = "../imagini/sun.svg"; c = true }
+    if (c) { col.src = "./imagini/moon.svg"; c = false; } else { col.src = "./imagini/sun.svg"; c = true }
 
 
 })
