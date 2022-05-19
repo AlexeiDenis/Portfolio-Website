815e1b3df36ca63c8b2180999d3a536f04c1ad49
AlexeiDenis <83495616+AlexeiDenis@users.noreply.github.com>
2022-05-19 15:20:27 +0300

    changed togglerdark

--- src/index.js
+++ src/index.js
@@ -1,14 +1,63 @@
-let btn = document.getElementById('toggler')
-let col = document.getElementById('decoration')
 let image = document.getElementById('imgdecor')
-let c = true;
 let y = true;
-
 
 image.addEventListener('click', () => {
     if (y) { y = false; image.src = "./imagini/sticker.webp" } else { y = true; image.src = "./imagini/avatar.webp" }
 })
 
-btn.addEventListener('click', () => {
-    if (c) { col.src = "./imagini/moon.svg"; c = false; } else { col.src = "./imagini/sun.svg"; c = true }
-})
+
+const storageKey = 'theme-preference'
+
+const onClick = () => {
+
+    theme.value = theme.value === 'light'
+        ? 'dark'
+        : 'light'
+
+    setPreference()
+}
+
+const getColorPreference = () => {
+    if (localStorage.getItem(storageKey))
+        return localStorage.getItem(storageKey)
+    else
+        return window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'dark'
+            : 'light'
+}
+
+const setPreference = () => {
+    localStorage.setItem(storageKey, theme.value)
+    reflectPreference()
+}
+
+const reflectPreference = () => {
+    document.firstElementChild
+        .setAttribute('data-theme', theme.value)
+
+    document
+        .querySelector('#toggler')
+        ?.setAttribute('aria-label', theme.value)
+}
+
+const theme = {
+    value: getColorPreference(),
+}
+
+
+reflectPreference()
+
+window.onload = () => {
+
+    reflectPreference()
+
+    document.getElementById('toggler').addEventListener('click', onClick)
+}
+
+
+window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({ matches: isDark }) => {
+        theme.value = isDark ? 'dark' : 'light'
+        setPreference()
+    })
